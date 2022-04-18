c24b5ef23d18391a0db1821845902c05b8e52fd6
rekybongso <rekybongso@gmail.com>
2022-04-18 23:58:12 +0800

    Modify footer

---
+++ src/component/icons/FBIcon.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const FBIcon = () => {
+	return (
+		<svg
+			width='36'
+			height='37'
+			viewBox='0 0 36 37'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'>
+			<g clipPath='url(#clip0_42_55)'>
+				<path
+					d='M0 18.5C0 8.5589 8.0589 0.5 18 0.5C27.9411 0.5 36 8.5589 36 18.5C36 28.4411 27.9411 36.5 18 36.5C8.0589 36.5 0 28.4411 0 18.5Z'
+					fill='#3B5998'
+				/>
+				<path
+					d='M19.8761 29.0836V19.2907H22.5794L22.9377 15.916H19.8761L19.8808 14.2269C19.8808 13.3467 19.9644 12.8751 21.2286 12.8751H22.9186V9.5H20.2149C16.9673 9.5 15.8243 11.1371 15.8243 13.8902V15.9163H13.8V19.291H15.8243V29.0836H19.8761Z'
+					fill='white'
+				/>
+			</g>
+			<defs>
+				<clipPath id='clip0_42_55'>
+					<rect
+						width='36'
+						height='36'
+						fill='white'
+						transform='translate(0 0.5)'
+					/>
+				</clipPath>
+			</defs>
+		</svg>
+	);
+};
+
+export default FBIcon;
--- src/container/footer/Footer.js
+++ src/container/footer/Footer.js
@@ -1,9 +1,97 @@
 import React from "react";
 
+import FBIcon from "../../component/icons/FBIcon";
+import IGIcon from "../../component/icons/IGIcon";
+import TwitterIcon from "../../component/icons/TwitterIcon";
+import YTIcon from "../../component/icons/YTIcon";
+import LocationIcon from "../../component/icons/LocationIcon";
+import logoMolacash from "../../assets/img/logo-molacash.png";
+
 const Footer = () => {
 	return (
 		<>
-			<div>Footer</div>
+			<div className='mt-9 grid grid-flow-row gap-5 bg-gray-300 p-7 md:gap-1 lg:grid-flow-col lg:grid-cols-5'>
+				<div>
+					<img
+						className='w-32'
+						src={logoMolacash}
+						alt="Molacash's company logo"
+					/>
+					<div className='flex flex-row gap-3'>
+						<div className='mt-3'>
+							<i>{LocationIcon()}</i>
+						</div>
+						<p className='mt-3 text-xs'>
+							Jl Suryo Pranoto No. 59 Petojo Selatan Gambir Jakarta Pusat DKI
+							Jakarta RT.10/RW.7, Petojo Utara Kecamatan Gambir, Kota Jakarta
+							Pusat Daerah Khusus Ibukota Jakarta 10160
+							<br />
+							<br />
+							Hotline : 085369694646 <br />
+							Email : customercare@molacash.co.id
+						</p>
+					</div>
+				</div>
+				<div>
+					<p className='font-bold'>Produk</p>
+					<ul className='mt-5 text-sm'>
+						<li>Pinjaman Tunai</li>
+						<li>Pinjaman Modal Usaha</li>
+						<li>Pinjaman Multiguna</li>
+					</ul>
+				</div>
+				<div>
+					<p className='font-bold'>Promo</p>
+					<ul className='mt-5 text-sm'>
+						<li>Promo Pinjaman Tunai</li>
+						<li>Promo Pinjaman Modal Usaha</li>
+						<li>Promo Pinjaman Multiguna</li>
+					</ul>
+				</div>
+				<div>
+					<p className='font-bold'>FAQ</p>
+					<ul className='mt-5 text-sm'>
+						<li>Terms & Conditions</li>
+						<li>Privacy Policy</li>
+					</ul>
+				</div>
+				<div className='w-fit'>
+					<p className='font-bold'>Follow media sosial kami</p>
+
+					<ul className='mt-5 flex flex-row justify-evenly gap-3'>
+						<li>
+							<a
+								href='https://id-id.facebook.com/'
+								target='_blank'
+								rel='noreferrer'>
+								<i>{FBIcon()}</i>
+							</a>
+						</li>
+						<li>
+							<a href='https://instagram.com/' target='_blank' rel='noreferrer'>
+								<i>{IGIcon()}</i>
+							</a>
+						</li>
+						<li>
+							<a href='https://twitter.com/' target='_blank' rel='noreferrer'>
+								<i>{TwitterIcon()}</i>
+							</a>
+						</li>
+						<li>
+							<a href='https://youtube.com/' target='_blank' rel='noreferrer'>
+								<i>{YTIcon()}</i>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div className='flex w-full flex-col items-center bg-gray-300'>
+				<hr className='w-11/12 border border-gray-600' />
+
+				<p className='p-7 text-sm lg:mr-6 lg:self-end'>
+					Copyright © 2022. All Rights Reserved
+				</p>
+			</div>
 		</>
 	);
 };
